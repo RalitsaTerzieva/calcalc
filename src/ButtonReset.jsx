2d90d902417d6f03c2d5547f83a12f8b2b916401
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2021-08-27 17:25:18 +0300

    Add heart icon on the reset button

--- src/ButtonReset.jsx
+++ src/ButtonReset.jsx
@@ -1,9 +1,10 @@
 //import React, { Component } from 'react';
 import './ButtonReset.css';
+import {BsFillHeartFill } from "react-icons/bs";
 
 
 function ButtonReset() {
-    return <button>Reset Fields</button>
+    return <button> <BsFillHeartFill /> Reset Fields</button>
 }
 
 export default ButtonReset;
